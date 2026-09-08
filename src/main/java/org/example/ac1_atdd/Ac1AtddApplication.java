fe91cc6f18bf702c5c20fd6343086e25c29716af
GustavoGomes <gomesgustavo1504@gmail.com>
2026-09-08 20:01:59 -0300

    first commit

---
+++ src/main/java/org/example/ac1_atdd/Ac1AtddApplication.java
@@ -0,0 +1,13 @@
+package org.example.ac1_atdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac1AtddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ac1AtddApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/ac1_atdd/domain/Aluno.java
@@ -0,0 +1,4 @@
+package org.example.ac1_atdd.domain;
+
+public class Aluno {
+}
